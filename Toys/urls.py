e5505cb6709c1b4eebc5f6d63240e4c19dd00278
Vasudha <koyavasudhalakshmi@email.com>
2021-05-11 09:34:57 +0530

    commitdone

--- Toys/urls.py
+++ Toys/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     
     path('image_upload/', hotel_image_view, name = 'image_upload'),
     path('success/', success, name = 'success'),
+    path('hotel_images', display_hotel_images, name = 'hotel_images'),
 
 
 
--- Toys/views.py
+++ Toys/views.py
@@ -63,3 +63,15 @@ def hotel_image_view(request):
   
 def success(request):
     return HttpResponse('successfully uploaded')
+
+
+# Python program to view
+# for displaying images
+
+def display_hotel_images(request):
+
+	if request.method == 'GET':
+
+		# getting all the objects of hotel.
+		Hotels = Hotel.objects.all()
+		return render(request, 'display_hotel_images.html',{'hotel_images' : Hotels})
